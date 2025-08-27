ff95184be0b14f86396f35c6226960f55bed26e0
samerspc <mas.norvg13@gmail.com>
2025-08-27 21:42:57 +0300

    feat: add header navigation

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,21 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
 import { AppHeaderUI } from '@ui';
+import { selectUser } from '../../services/slices/userSlice';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+export const AppHeader: FC = () => {
+  const user = useSelector(selectUser);
+  const location = useLocation();
+
+  const userName = user?.name;
+
+  return (
+    <AppHeaderUI
+      userName={userName}
+      isConstructorActive={location.pathname === '/'}
+      isFeedActive={location.pathname === '/feed'}
+      isProfileActive={location.pathname.startsWith('/profile')}
+    />
+  );
+};
